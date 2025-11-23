ebf44ad2556be01ad80913e4278e027989987003
Richard Pham <phamrichard45@gmail.com>
2025-11-23 03:30:07 -0500

    add another test for file<quant.cng>

--- tests/test_cng.py
+++ tests/test_cng.py
@@ -28,6 +28,20 @@ class CLCGClass(unittest.TestCase):
                         109, 82, 2, 2, 3, 6, 15, 0, 0, -3, \
                         109, 82, 4, 4, 9, 24, 69, 3, 55]
 
+    def test__CLCG__next__case2(self):
+        start = 14 
+        m = -3
+        a = -7 
+        n0,n1 = -100,111 
+        lx = prg__LCG(56,3,11,1111)
+        cg = CLCG(start,m,a,n0,n1,55,7,1,lx)
+
+        q = [] 
+        for i in range(100): 
+            q.append(next(cg)) 
+
+        assert q[56:] == ([7651, 7, 28, 91, 280, 847, 2548] * 7)[:-5]
+
 
 if __name__ == '__main__':
     unittest.main()
