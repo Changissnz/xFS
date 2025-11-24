f327aef83a6bb3146bde1be0c775176e1bcefe7f
Richard Pham <phamrichard45@gmail.com>
2025-11-24 15:49:57 -0500

    refactor file<quant.qstruct> to file<graph_models.rnb_struct>

--- graph_models/rnb_struct.py
+++ graph_models/rnb_struct.py
@@ -2,11 +2,12 @@ import numpy as np
 
 class QStruct:
 
-    def __init__(self,dim):
+    def __init__(self,dim,answers:dict): 
         assert len(dim) == 2
         assert type(dim[0]) == type(dim[1]) 
         assert min(dim) > 0 and type(dim[0]) == int 
         self.dim = dim 
+        self.answers = answers 
         self.init_mat() 
     
     def init_mat(self): 
@@ -16,10 +17,24 @@ class QStruct:
         self.crate = np.zeros(self.dim) 
         # question frequency rate 
         self.frate = np.zeros(self.dim) 
-        # average answer rate 
+        # average answers 
         self.arate = np.zeros(self.dim)  
         return
 
     def update(self,node_idn,q_idn,answer):
         f = self.frate[node_idn,q_idn]
         self.frate[node_idn,q_idn] += 1 
+
+class RStruct: 
+
+    def __init__(self,node_idn,answers:dict,answer_objective:dict):  
+        self.node_idn = node_idn
+        self.answers = answers 
+        self.answer_objective = answer_objective
+        return
+    
+class RNet:
+
+    def __init__(self,d:dict,rstruct_map,q):
+
+        return -1 
--- quant/qagroup_mat.py
+++
@@ -1,8 +0,0 @@
-import numpy as np 
-
-class QAGMat:
-
-    def __init__(self,X:np.ndarray):
-        assert type(X) == np.ndarray 
-        self.X = X 
-        return 
