ec93a32402561e0dd876e9b6f1a9195fb03fe40a
Richard Pham <phamrichard45@gmail.com>
2025-12-27 18:27:05 -0500

    start work on file<graph_problems.hte>

--- graph_problems/hte.py
+++ graph_problems/hte.py
@@ -1,11 +1,17 @@
 """
 Hidden Threat Exposure walkthroughs  
 """
+from quant.hte_aux import * 
+from quant.hte_navigator import* 
 
 class HTEBot:
 
-    def __init__(self): 
-        print("UP") 
+    def __init__(self,hte_surface,hte_navigator):  
+        assert hte_surface == HTESurface
+        assert hte_navigator == HTENavigator
+        self.hte_surface = hte_surface
+        self.hte_navigator = hte_navigator
         return 
 
+    
     
