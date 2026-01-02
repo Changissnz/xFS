4828bf723aee70d504cd3f3f7200fc27ddc668bc
Richard Pham <phamrichard45@gmail.com>
2026-01-02 07:37:36 -0500

    little work on file<quant.hte_navigator>

--- quant/hte_navigator.py
+++ quant/hte_navigator.py
@@ -3,6 +3,26 @@ from .hte_analog_induction import *
 
 DEFAULT_HTE_VISUAL_RADIUS = 3 
 
+class HTENavigatorPrediction:
+
+    def __init__(self,reference_graph,threat_nodes,prg):
+        self.reference_graph = reference_graph
+        self.threat_nodes = threat_nodes 
+        self.prg = prg 
+        return
+
+    def possible_threats(self,context:defaultdict,loc,radius):
+        d = {t:(loc,radius) for t in self.threat_nodes}
+
+        hai = HTEAnalogInducer(self.reference_graph,context,self.threat_nodes,\
+            next_graph_hyp_map=d, isomorphic_subgraph_radius_range=\
+            DEFAULT_ANALOG_GRAPH_SUBGRAPH_RADIUS_RANGE,prg = self.prg)
+
+        possible = set() 
+        for t in self.threat_nodes: 
+            possible |= hai.possible_threat_analogs(t) 
+        return possible 
+
 class HTENavigator(NodeObjectiveNavigator): 
 
     def __init__(self,loc,avoid_nodeset,take_nodeset,objective_nodeset,prg,\
@@ -15,6 +35,7 @@ class HTENavigator(NodeObjectiveNavigator):
         self.visual_radius = visual_radius
         self.visual_of_graph = defaultdict(set)
         self.fin_stat = False 
+        self.hnp = None 
         return
 
     def __next__(self): 
@@ -24,6 +45,9 @@ class HTENavigator(NodeObjectiveNavigator):
             return 
 
         return self.make_choice() 
+
+    def load_previous_HTE_data(self,reference_graph,threat_nodes): 
+        self.hnp = HTENavigatorPrediction(reference_graph,threat_nodes,self.prg)
     
     def receive_context(self,graph_visual:defaultdict): 
         assert self.loc in graph_visual
@@ -32,4 +56,12 @@ class HTENavigator(NodeObjectiveNavigator):
         self.visual_of_graph = (MicroGraph(self.visual_of_graph) + \
             MicroGraph(graph_visual)).dg
 
-    
+        self.predict_threats() 
+
+    # CAUTION: overcautious? 
+    def predict_threats(self): 
+        if type(self.hnp) = type(None): return 
+
+        possible = self.hnp.possible_threats(self.context,self.loc,self.visual_radius)
+        self.avoid |= possible 
+        return possible 
