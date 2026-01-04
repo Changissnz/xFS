9986a9fb2462c7b166b3069c34edf074cbb4cad6
Richard Pham <phamrichard45@gmail.com>
2026-01-04 04:15:32 -0500

    finished first draft for class<HTEBot>; testing next.

--- graph_problems/hte.py
+++ graph_problems/hte.py
@@ -6,6 +6,7 @@ from quant.hte_navigator import*
 
 DEFAULT_NAVIGATOR_NODE_FUEL_MULTIPLIER = 1.0 
 
+# TODO: test. 
 """
 Hidden Threat Exposure automaton is related to the 1990's video 
 game, Minesweeper. See this wikipedia article, 
@@ -18,19 +19,78 @@ a network), in addition to the problem of selecting nodes that are not
 threats for travel over. The use of cycling variants, as part of 
 trial-and-error discovery, in Hidden Threat Exposure traversal decisions
 are a kind of machine-learning technique. 
+----------------------------------------------------------------------------
+
+The <HTENavigator> is a subclass of <NodeObjectiveNavigator>, a structure 
+that uses these four node classifications to prioritize which node to travel 
+to during the course of navigation:
+- objective (ideal node to be on)
+- take (a node that the navigator classified as safe to travel over)
+- possible avoid (a node that the navigator classified as a possible threat node)
+- avoid (a node that the navigator classified as a certain threat node).
+<HTENavigator> uses a stochastic process to make decisions revolving around these 
+four node classifications. For a starting node n0 and objective node n1, one <HTENavigator> 
+may take a path P from n0 to n1, but a succeeding <HTENavigator> may not take the same 
+path P despite starting at same node n0. 
+
+If two nodes are of the same classification, the <NodeObjectiveNavigator> prioritizes 
+the node less frequently travelled, according to its node encounters map,  
+    encountered node -> frequency of travel.
+
+A navigator travels a surface (network) with threats of unknown location (node) 
+on it. Navigator terminates when it travels into a threat node (loss) or an objective 
+node (win) or when it runs out of fuel (loss). When a navigator terminates, <HTEBot> can 
+summon another navigator instance for the same surface. This navigator instance may have 
+access to the previous navigator's node encounters map, depending on <HTEBot> variable 
+`navigator_remembers_past_encounters`. If the navigator is `memory_less`, it does not store 
+the nodes of the paths it took into memory, the nodes without threats on them into the 
+`take` nodeset, in other words. The next summoned navigator for the same <HTESurface> will 
+not know the nodes the previous navigator took. 
+
+When <HTESurface> 'reproduces` a new <HTESurface> from a previous one, this new <HTESurface> 
+is an isomorphic derivation of the previous surface. So the threat nodes of the previous 
+surface may translate into those of similar geometric property to this new <HTESurface>. 
+If `navigator_uses_isomorphic_prediction` is set to True, the <HTENavigator> will attempt 
+to predict threat nodes on the new <HTESurface> before proceeding to traveling it. 
+
+From the navigator perspective, there are three mode classes: 
+- navigator_remembers_past_encounters
+- navigator_uses_isomorphic_prediction
+- memory_less navigator (see function<set_memoryless_navigator>)
+
+A <HTEThreat> may be one of two categories, contra or constant. When a navigator encounters 
+a <HTEThreat>, the navigator terminates and the lifespan of the <HTEThreat> reduces by 1 
+(<HTEThreat> terminates when its lifespan decreases to 0). If the <HTEThreat> is still 
+active after the navigator encounters it, <HTEThreat> will stay at the same node if it is 
+constant. If <HTEThreat> is contra, it will attempt to relocate to another node that satisfies 
+the following conditions: 
+- new node location does not have an existing threat on it, 
+- new node location is a node on the path the terminated navigator took to get to the 
+  original threat location. 
+A contra <HTEThreat> that cannot find a new node location that satisfies these two conditions 
+will stay at the original node location. 
+
+There are two types of reproductions in <HTEBot>, one for the <HTESurface> and one for the 
+<HTENavigator>. 
 """
 class HTEBot:
 
-    def __init__(self,hte_surface,hte_navigator,verbose=False):  
+    def __init__(self,hte_surface,hte_navigator,navigator_remembers_past_encounters:bool=False,\
+        navigator_uses_isomorphic_prediction:bool=True,verbose=False):  
         assert type(hte_surface) == HTESurface
         assert type(hte_navigator) in {type(None),HTENavigator} 
 
         self.hte_surf = hte_surface
+        self.hte_surf_prior_ref = None 
         self.hte_nav = hte_navigator
+        self.navigator_remembers = navigator_remembers_past_encounters
         self.verbose = verbose 
+        self.previous_hsurfaces = [] 
+        self.hsurface_prior_ref = [] 
         self.terminated_navigators = [] 
 
         self.preproc() 
+        self.hte_nav.uses_isomorphic_prediction = navigator_uses_isomorphic_prediction 
         self.hte_nav.fuel = DEFAULT_NAVIGATOR_NODE_FUEL_MULTIPLIER * len(self.hte_surf.base_graph)
         return 
 
@@ -52,6 +112,7 @@ class HTEBot:
         self.register_threat_contact(self.hte_nav.loc)
         return 
 
+    #-------------------------------------------- methods for node navigator 
     def run_navigator(self): 
         if self.verbose: print("-- navigator at node={}, fuel={}".format(self.hte_nav.loc,self.hte_nav.fuel))
 
@@ -60,6 +121,10 @@ class HTEBot:
             if self.verbose: print("-- navigator at node={}, fuel={}".format(self.hte_nav.loc,self.hte_nav.fuel))
         if self.verbose: print("-- status: ",self.hte_nav.success_stat)
         return
+
+    def set_memoryless_navigator(self,stat:bool=True):
+        assert type(stat) == bool  
+        self.hte_nav.memory_less = stat  
 
     def __next__(self): 
         if self.hte_nav.fin_stat: 
@@ -121,13 +186,15 @@ class HTEBot:
         self.hte_surf.update_threat_activation(l,pertinent_path_info,threats)
         return True 
 
-    #----------------------------------------------------------------------------------------------
+    #--------------------------- methods to produce succeeding navigators and threat surfaces
 
-    def reproduce_terminated_navigator(self):
-        if not self.hte_nav.fin_stat: return 
+    def reproduce_terminated_navigator(self,next_full_context=None):
 
         epoint = self.choose_entry_point_for_navigator() 
-        hten = self.hte_nav.reproduce(epoint,iso_predict_mode=True)  
+        hten = self.hte_nav.reproduce(epoint,next_full_context=next_full_context)  
+        if self.navigator_remembers and type(next_full_context) == type(None): 
+            hten.encountered = self.hte_nav.encountered 
+            hten.encountered[epoint] += 1 
 
         self.terminated_navigators.append(self.hte_nav) 
         self.hte_nav = hten 
@@ -135,6 +202,29 @@ class HTEBot:
         self.register_threat_contact(self.hte_nav.loc)
         self.hte_nav.fuel = DEFAULT_NAVIGATOR_NODE_FUEL_MULTIPLIER * len(self.hte_surf.base_graph)
         if self.verbose: print("** reproducing navigator at entry={}".format(self.hte_nav.loc)) 
+        return
+
+    def reproduce_surface(self): 
+        htes2,isomap = self.hte_surf.prng_reproduction_scheme2() 
+
+        isomap_hyp = {} 
+        nav_avoid = self.hte_nav.avoid 
+        for k,v in isomap.items(): 
+            if k not in nav_avoid: 
+                continue 
+
+            isomap_hyp[k] = (v,modulo_in_range(\
+                int(self.hte_surf.prg()),DEFAULT_ANALOG_GRAPH_SUBGRAPH_RADIUS_RANGE))
+            
+        self.previous_hsurfaces.append(self.hte_surf) 
+        if type(self.hte_surf_prior_ref) != type(None): 
+            self.hsurface_prior_ref.append(self.hte_surf_prior_ref) 
+
+        self.hte_surf = htes2 
+        self.hte_surf_prior_ref = isomap 
+
+        next_full_context = (self.hte_surf.base_graph,isomap_hyp,self.hte_surf.objective_points)
+        self.reproduce_terminated_navigator(next_full_context=next_full_context)
         return
 
     def choose_entry_point_for_navigator(self): 
--- quant/hte_navigator.py
+++ quant/hte_navigator.py
@@ -10,13 +10,34 @@ class HTENavigatorPrediction:
         self.reference_graph = reference_graph
         self.threat_nodes = threat_nodes 
         self.prg = prg 
+        self.suspected_threat_nodes = set()  
         return
 
-    def possible_threats(self,context:defaultdict,loc,radius):
-        d = {t:(loc,radius) for t in self.threat_nodes}
+    """
+    NOTE: this prediction route involves reviewing all known threat nodes from 
+          previous context for the current partial context, a subgraph centered 
+          around navigator location `loc` with an arbitrary `radius`. 
 
-        hai = HTEAnalogInducer(self.reference_graph,context,self.threat_nodes,\
-            next_graph_hyp_map=d, isomorphic_subgraph_radius_range=\
+          For usage, this method would be called every time navigator travels to 
+          another node, which could be non-unique. Due to this logistic, method is 
+          slow and computationally intensive. 
+    """
+    def possible_threats(self,context:defaultdict,loc,radius,store_results:bool=False):
+        d = {t:(loc,radius) for t in self.threat_nodes}
+        return self.full_possible_threats_for_next_context(context,d,store_results)
+        
+    """
+    NOTE: this prediction route takes a `next_context`, the full graph for the next 
+          <HTESurface>, and makes predictions on the threat nodes of this `next_context`. 
+          This is a one-time method called after <HTEBot> updates its <HTESurface>. It 
+          is quicker than the approaching of a navigator calling function<possible_threats> 
+          every time it travels to a next node. 
+    """
+    def full_possible_threats_for_next_context(self,next_context,next_context_hyp_map,\
+        store_results:bool=False): 
+
+        hai = HTEAnalogInducer(self.reference_graph,next_context,self.threat_nodes,\
+            next_context_hyp_map, isomorphic_subgraph_radius_range=\
             DEFAULT_ANALOG_GRAPH_SUBGRAPH_RADIUS_RANGE,prg = self.prg)
 
         possible = set() 
@@ -28,14 +49,18 @@ class HTENavigatorPrediction:
                 print("NICHT ",t)
                 print(set(self.reference_graph.keys()))
                 pass 
+        
+        if store_results: 
+            self.suspected_threat_nodes = deepcopy(possible) 
         return possible 
+
 
 class HTENavigator(NodeObjectiveNavigator): 
 
     def __init__(self,loc,avoid_nodeset,take_nodeset,objective_nodeset,prg,\
         path_log_length=float('inf'),absolute_avoid:bool=False,\
         visual_radius=DEFAULT_HTE_VISUAL_RADIUS,contra_risk=DEFAULT_CONTRA_RISK,\
-        fuel=float('inf')): 
+        fuel=float('inf'),memory_less:bool=False,uses_isomorphic_prediction:bool=False): 
         
         super().__init__(loc,avoid_nodeset,take_nodeset,objective_nodeset,\
             prg,path_log_length,absolute_avoid) 
@@ -43,11 +68,24 @@ class HTENavigator(NodeObjectiveNavigator):
         self.visual_radius = visual_radius
         self.contra_risk = contra_risk 
         self.fuel = fuel 
+        self.memory_less = memory_less
+        self.uses_isomorphic_prediction = uses_isomorphic_prediction
         self.visual_of_graph = defaultdict(set)
         self.fin_stat = False 
         self.success_stat = False 
         self.hnp = None 
         return
+
+    def __str__(self): 
+
+        S = "** navigator info\n"
+        S += "- visual of graph: {}\n".format(len(self.visual_of_graph)) 
+        S += "- avoid: {}\n".format(len(self.avoid))
+        S += "- possible avoid: {}\n".format(len(self.possible_avoid))
+        S += "- take: {}\n".format(len(self.take)) 
+        S += "- path length: {} unique nodes: {}\n".format(len(self.path_log),len(set(self.path_log)))  
+        S += "- success: {}\n".format(self.success_stat)
+        return S 
 
     def __next__(self): 
         if self.fin_stat: return 
@@ -58,12 +96,21 @@ class HTENavigator(NodeObjectiveNavigator):
         if self.loc in self.objectives: 
             return 
 
+        # case: contra node, update status of node as safe
+        ##if self.loc in self.avoid: 
+        ##    self.avoid -= {self.loc} 
+
         l = self.make_choice() 
         self.fuel -= 1 
         return l 
 
     def load_previous_HTE_data(self,reference_graph,threat_nodes): 
         self.hnp = HTENavigatorPrediction(reference_graph,threat_nodes,self.prg)
+        return 
+
+    def full_navigator_prediction(self,next_context,next_context_hyp_map): 
+        self.hnp.full_possible_threats_for_next_context(next_context,next_context_hyp_map,True) 
+        self.possible_avoid |= self.hnp.suspected_threat_nodes
     
     def load_previous_visual_of_graph(self,reference_graph): 
         assert type(reference_graph) == defaultdict 
@@ -76,7 +123,7 @@ class HTENavigator(NodeObjectiveNavigator):
         self.visual_of_graph = (MicroGraph(self.visual_of_graph) + \
             MicroGraph(graph_visual)).dg
 
-        self.predict_threats() 
+        ##self.predict_threats() 
 
     # CAUTION: overcautious? 
     def predict_threats(self): 
@@ -87,7 +134,8 @@ class HTENavigator(NodeObjectiveNavigator):
         return possible 
 
     def made_contact(self): 
-        self.avoid |= {self.loc}
+        if not self.memory_less: 
+            self.avoid |= {self.loc}
         self.mark_finish() 
 
     def made_objective(self): 
@@ -97,33 +145,53 @@ class HTENavigator(NodeObjectiveNavigator):
     def mark_finish(self): 
         self.fin_stat = True 
 
-    def reproduce(self,new_entry_loc,iso_predict_mode): 
-        if not self.fin_stat: 
-            print("cannot reproduce active navigator")
-            return 
+    def reproduce(self,new_entry_loc,next_full_context): 
 
-        # declare new 
-        hten = HTENavigator(new_entry_loc,self.avoid,self.take-self.avoid,self.objectives,\
-            self.prg,path_log_length=float('inf'),absolute_avoid=self.absolute_avoid,\
-            visual_radius=self.visual_radius,contra_risk=self.contra_risk)
-        hten.load_previous_visual_of_graph(self.visual_of_graph)
-        hten.add_possible_avoid(self.possible_avoid)
-
-        take = set(hten.path_log) - {self.loc}
-        # if terminated by threat, add possible avoid 
-        if not self.success_stat: 
-            possible_avoid = set(self.context.keys()).intersection(set(self.path_log)) - {self.loc} 
+        # case: same surface 
+        if type(next_full_context) == type(None): 
+            avoid = self.avoid 
+            take = self.take - self.avoid 
+            objectives = self.objectives 
+        # case: different surface 
+        else: 
+            avoid = set() 
             take = set() 
+            objectives = next_full_context[2] 
+
+        hten = HTENavigator(new_entry_loc,avoid,take,objectives,\
+            self.prg,path_log_length=float('inf'),absolute_avoid=self.absolute_avoid,\
+            visual_radius=self.visual_radius,contra_risk=self.contra_risk,\
+            memory_less=self.memory_less,uses_isomorphic_prediction=self.uses_isomorphic_prediction)        
+
+        take = set() 
+        # case: same surface, load cumulative visual of graph and nodes of status `possible avoid`.
+        if type(next_full_context) == type(None): 
+            hten.load_previous_visual_of_graph(self.visual_of_graph)
+            hten.add_possible_avoid(self.possible_avoid)
+            take = self.take | set(hten.path_log) - {self.loc} \
+                if not self.memory_less else set() 
+
+        # case: same surface, if terminated by threat, add possible avoid 
+        if not self.success_stat and not self.memory_less and type(next_full_context) == type(None): 
+            possible_avoid = set(self.context.keys()).intersection(set(self.path_log)) - {self.loc} 
+            take = self.take - set(hten.path_log)
             if len(possible_avoid) > 0: 
                 possible_avoid = prg_seqsort(sorted(possible_avoid),self.prg) 
                 la = ceil((1-self.contra_risk) * len(possible_avoid))
                 q = set(possible_avoid[:la])
-                take = set(possible_avoid[la:]) 
+                take = take | set(possible_avoid[la:]) 
                 possible_avoid = q 
-
             hten.add_possible_avoid(possible_avoid) 
         hten.add_take(take) 
 
-        # add previous HTESurface data if `iso_predict_mode`
-        if iso_predict_mode: hten.load_previous_HTE_data(self.visual_of_graph,self.avoid)
+        # case: new surface, use previous <HTESurface> data to make threat predictions if 
+        #       `uses_isomorphic_prediction`
+        if type(next_full_context) != type(None) and hten.uses_isomorphic_prediction: 
+            assert type(next_full_context) == tuple and len(next_full_context) == 3
+            assert type(next_full_context[0]) == defaultdict 
+            assert type(next_full_context[1]) == dict 
+            ##print("PPPPPPPPPPPPPPPPPPPPPPP")
+            hten.load_previous_HTE_data(self.visual_of_graph,self.avoid) 
+            hten.full_navigator_prediction(next_full_context[0],next_full_context[1])            
+
         return hten
