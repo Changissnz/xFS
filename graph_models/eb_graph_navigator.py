528a5c88d37274c46a7e1ca548681d53fe993fcc
Richard Pham <phamrichard45@gmail.com>
2026-02-03 20:50:03 -0500

    .

--- graph_models/eb_graph_navigator.py
+++ graph_models/eb_graph_navigator.py
@@ -10,10 +10,13 @@ DEFAULT_BULLKILLER_AGENT_MODES = {\
 Graph navigator operates on finite energy. Once energy 
 reaches non-positive real number, navigator terminates. 
 
-Max speed is the upper threshold for navigator to travel 
-weighted edges. If navigator lands on an edge (u,v) instead 
-of node u or v, algorithm rounds navigator location down to 
+If navigator lands on an edge (u,v) instead of node u or v, 
+algorithm rounds navigator location down to 
 node u. 
+
+There are two possible roles this navigator can be: 
+- Bull: attempts to maximize distance to Chasers 
+- Chaser: attempts to be at the same node as Bull. 
 
 Used for Bull Killer Simulation. 
 """
