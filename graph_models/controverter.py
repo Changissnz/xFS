300b1dac13ede3cc61337ff5435774763a53d4dc
Richard Pham <phamrichard45@gmail.com>
2026-03-13 19:57:45 -0400

    add 3 files in folder<graph_models> to work on

---
+++ graph_models/controverter.py
@@ -0,0 +1,4 @@
+class GameControverter:
+
+    def __init__(self): 
+        return -1 
---
+++ graph_models/modular_graph.py
@@ -0,0 +1,6 @@
+from .radial_subgraph import * 
+
+class ModularGraph:
+
+    def __init__(self,base_graph): 
+        return -1 
