2b76231d74277a0bbcecbe30c725319d6babaf5b
Richard Pham <phamrichard45@gmail.com>
2026-05-31 07:48:32 -0400

    just a little bit more in file<quant.agent_23>

--- quant/agent_23.py
+++ quant/agent_23.py
@@ -1,3 +1,33 @@
+
+DEFAULT_AGENT_TYPE_2F3M_MODUS_OPERANDI_TYPES = {"compatible characterization","third-party contra"} 
+
+class AgentType2F3MMOContainer: 
+
+    def __init__(self,agent_idn,mo_type,cat2label_map,compatibility_map):
+        assert mo_type in DEFAULT_AGENT_TYPE_2F3M_MODUS_OPERANDI_TYPES
+        assert len(cat2label_map) > 0 
+        assert type(cat2label_map) in {dict,defaultdict} 
+        for v in cat2label_map.values(): assert type(v) == list 
+
+        assert len(compatibility_map) > 0 
+        for v in compatibility_map.values(): assert is_number(v) 
+
+        self.agent_idn = agent_idn 
+        self.mo_type = mo_type 
+        self.c2l_map = cat2label_map 
+        self.comp_map = compatibility_map
+        return 
+
+    def characterize_(self,other_agent:AgentType2F3M,category): 
+        assert type(other_agent) == AgentType2F3M
+        assert other_agent != self 
+        assert other_agent.idn in self.comp_map 
+
+
+    def justify(self,other_agent:AgentType2F3M): 
+
+        return -1 
+
 """
 Agent Type 2 (F)aces 3 (M)otives. 
 
@@ -5,6 +35,6 @@ Agent Type 2 (F)aces 3 (M)otives.
 """
 class AgentType2F3M: 
 
-    def __init__(self): 
-
-        return -1 
+    def __init__(self,idn,mo_container:AgentType2F3MMOContainer):    
+        self.idn = idn 
+        self.mo_container = mo_container
